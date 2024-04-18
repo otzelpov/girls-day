8530a949f79e1c1c0cc5a2aa1ea540ebf5ef07ed
Alex Otsepaev <alex.otsepaev@diva-e.com>
2024-04-18 14:53:44 +0200

    init commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+function clickButtonOne() {
+  let text = "Für mehr Infos gehen sie bitte auf die Webseite des Tierheims​";
+  alert(text);
+  console.log(text);
+}
+
+function clickButtonTwo() {
+  // toDo: Funktion schreiben, die eine Alert-Box öffnet und mehrere Infos zu Abby enthält. (siehe PPP Folie 16)
+  // Idealerweise sind die Sätze durch Zeilenumbrüche getrennt
+
+  // https://dog.ceo/api/breeds/image/random ggf als Zusatzfaufgabe?
+}
